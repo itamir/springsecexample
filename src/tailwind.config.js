ae375ceab401c99cb5f42fbe44f771d05dd98e84
Itamir Filho <itamir.filho@gmail.com>
2024-02-01 15:14:56 -0300

    tailwind adition

---
+++ src/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./**/*.html",
+    "./**/*.js",
+    "./**/style.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+      extend: {},
+    },
+  plugins: [],
+}
+
